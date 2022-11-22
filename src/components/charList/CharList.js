a7a1fa47f2a9bcea0d39691ca8f6a969feae3acc
Andrey <samoilenkoand@gmail.com>
2022-11-22 20:36:42 +0300

    install & use proptypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -102,6 +104,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func
 }
 
 export default CharList;
