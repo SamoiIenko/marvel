ee6e2d0e35dfe0d66dc2e3d696ebd0f7d9d3ba79
Andrey <samoilenkoand@gmail.com>
2022-12-08 17:36:45 +0300

    create function to getting comics on id

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,6 +24,11 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    }
+
 
 
     const _transformCharacter = (char) => {
