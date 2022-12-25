41063e75975b68b64824f1490fef355a12a050ab
Andrey <samoilenkoand@gmail.com>
2022-12-25 23:18:25 +0300

    new func getCharacterByName in MarvelService

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,6 +24,11 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters/name=${name}&${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
+
     const getComics = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
@@ -55,7 +60,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getAllComicses, getCharacter, getComics}
+    return {loading, error, clearError, getAllCharacters, getAllComicses, getCharacter, getCharacterByName, getComics}
 }
 
 export default useMarvelService;
