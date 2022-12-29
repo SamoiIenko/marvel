0c23ca2716ce065a5942301ffdf3bdd5653ee26e
Andrey <samoilenkoand@gmail.com>
2022-12-29 20:46:47 +0300

    Add banner in Single page

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -5,6 +5,7 @@ import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import './singleComicPage.scss';
+import AppBanner from '../appBanner/AppBanner';
 
 const SingleComicPage = () => {
     const {comicId} = useParams(); 
@@ -42,7 +43,9 @@ const View = ({comic}) => {
     const {title, description, pageCount, thumbnail, price, language} = comic;
  
     return (
-        <div className="single-comic">
+        <>
+            <AppBanner />
+            <div className="single-comic">
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
@@ -53,6 +56,7 @@ const View = ({comic}) => {
             </div>
             <Link to="/comics" className="single-comic__back">Back to all</Link>
         </div>
+        </>
     )
 }
 
