7508eac6b3b181f998375ce23a5e501c32afa6e6
Andrey <samoilenkoand@gmail.com>
2023-01-02 23:25:51 +0300

    fsm for charSearchForm

--- src/components/CharSearchForm/CharSearchForm.js
+++ src/components/CharSearchForm/CharSearchForm.js
@@ -10,7 +10,7 @@ import './CharSearchForm.scss';
 
 function CharSearchForm() {
     const [char, setChar] = useState(null);
-    const {loading, error, getCharacterByName, clearError} = useMarvelService();
+    const {getCharacterByName, clearError, process, setProcess} = useMarvelService();
   
   
 
@@ -22,11 +22,12 @@ function CharSearchForm() {
         clearError();
 
         getCharacterByName(name)
-            .then(onCharLoaded);
+            .then(onCharLoaded)
+            .then(() => setProcess('confirmed'))
     }
 
 
-    const errorMessage = error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
+    const errorMessage = process === 'error' ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
     const results = !char ? null : char.length > 0 ? 
                     <div className="char__search-wrapper">
                         <div className="char__search-success">There is! Visit {char[0].name} page?</div>
@@ -62,7 +63,7 @@ function CharSearchForm() {
                             <button 
                                 type='submit' 
                                 className="button button__main"
-                                disabled={loading}>
+                                disabled={process === 'loading'}>
                                 <div className="inner">find</div>
                             </button>
                         </div>
